6dc697c4093ffa1b878add3e19e7a84a21bb7952
luiz.felipe <luiz.felipe@mag-it.com.br>
2021-07-07 23:58:43 -0300

    Add mapaDeBits e inicialização MemoryManager

--- projeto/Aux.java
+++ projeto/Aux.java
@@ -3,7 +3,7 @@ package projeto;
 
 public class Aux {
     public static void main(String[] args) {
-        MemoryManager mm = new MemoryManager(6);
+        MemoryManager mm = new MemoryManager(32);
         int x = mm.loadProcessToMemory("a");
         System.out.println("X : " + x);
     }
--- projeto/MemoryManager.java
+++ projeto/MemoryManager.java
@@ -3,14 +3,25 @@ package projeto;
 import exceptions.*;
 
 public class MemoryManager implements ManagementInterface {
-    public int qtdQuadrosASeremGerenciados; // Múltiplo de 32
-    //public HashMap<bit,unidadeDeAlocacao> mapaDeBits; // ?
+    public int qtdQuadrosASeremGerenciados; // 32/64/128
+    public int[] mapaDeBits; // Pode ter tamanho = 32/64/128
 
     //public TabelaDePágina[] listaTabelasDePáginas;
+
+    /*
+    Cada programa virtual:
+    - Texto : (segmento de texto com tamanho int > 1 e <= 960 bytes)
+    - Dados : (segmento de dados com tamanho int >= 0 e <= 928 bytes)
+    - Pilha : (segmento de pliha com tamanho == 64 bytes)
+    -- Tamanho máximo de 1024 bytes
+    */
 
 
     public MemoryManager(int qtdQuadrosASeremGerenciados) {
         this.qtdQuadrosASeremGerenciados = qtdQuadrosASeremGerenciados;
+        this.mapaDeBits = new int[qtdQuadrosASeremGerenciados];
+        for (int i = 0; i < this.mapaDeBits.length; i++) // Talvez não seja tudo inicializado como 0
+            this.mapaDeBits[i] = 0;
     }
 
     @Override
